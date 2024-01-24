72601ea0e0ad326a9a5d2efa3f30ac71be529180
oliveirascof <oliveirascof@gmail.com>
2024-01-23 23:08:20 -0300

    refactor: dev.db

--- src/@types/questions/index.ts
+++ src/@types/questions/index.ts
@@ -2,9 +2,9 @@ export interface QuestionsProps {
   enun: string
   a1: string 
   a2: string 
-  a3: string 
-  a4: string 
-  a5: string
+  a3?: string 
+  a4?: string 
+  a5?: string
   resp: string
   area: string
   materia: string
